2a51256102032f701bc00820538b8c2de919d5a9
riccardocioria <riccardocioria@gmail.com>
2011-06-03 10:42:35 +0000

    Aggiunta possibilità di cambiare layout Spinner/AutoComplete

--- src/it/vesuviana/servizi/db/OfflineDbOpenHelper.java
+++ src/it/vesuviana/servizi/db/OfflineDbOpenHelper.java
@@ -1,5 +1,6 @@
 package it.vesuviana.servizi.db;
 
+import it.vesuviana.servizi.model.Preference;
 import it.vesuviana.servizi.model.Stazioni.Stazione;
 
 import java.sql.SQLException;
@@ -14,11 +15,13 @@ import android.util.Log;
 
 public class OfflineDbOpenHelper extends OrmLiteSqliteOpenHelper {
 	private static final String DATABASE_NAME = "JPA_vesuviana.db";
-	private static final int DATABASE_VERSION = 7;
-	private static final String STAZIONI_TABLE_NAME = "stazioni";
+	private static final int DATABASE_VERSION = 8;
+	private static final String STAZIONI_TABLE_NAME = "stazione";
+	private static final String PREFERENCES_TABLE_NAME = "preference";
 	
-	// the DAO object we use to access the SimpleData table
+	// the DAO objects we use to access the SimpleData table
 	private Dao<Stazione, Integer> stazioneDAO = null;
+	private Dao<Preference, String> preferencesDAO = null;
 
 
 	public OfflineDbOpenHelper(Context context) {
@@ -29,15 +32,29 @@ public class OfflineDbOpenHelper extends OrmLiteSqliteOpenHelper {
 		return DATABASE_NAME;
 	}
 
+	/**
+	 * @return the stazioniTableName
+	 */
 	public static String getStazioniTableName() {
 		return STAZIONI_TABLE_NAME;
 	}
 
+	/**
+	 * @return the preferencesTableName
+	 */
+	public static String getPreferencesTableName() {
+		return PREFERENCES_TABLE_NAME;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase, com.j256.ormlite.support.ConnectionSource)
+	 */
 	@Override
 	public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
 		try {
 			Log.i(OfflineDbOpenHelper.class.getName(), "onCreate");
 			TableUtils.createTable(connectionSource, Stazione.class);
+			TableUtils.createTable(connectionSource, Preference.class);
 
 			long millis = System.currentTimeMillis();
 			
@@ -48,15 +65,16 @@ public class OfflineDbOpenHelper extends OrmLiteSqliteOpenHelper {
 		}
 	}
 	
-	/**
-	 * This is called when your application is upgraded and it has a higher version number. This allows you to adjust
-	 * the various data to match the new version number.
+
+	/* (non-Javadoc)
+	 * @see com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper#onUpgrade(android.database.sqlite.SQLiteDatabase, com.j256.ormlite.support.ConnectionSource, int, int)
 	 */
 	@Override
 	public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVersion, int newVersion) {
 		try {
 			Log.i(OfflineDbOpenHelper.class.getName(), "onUpgrade");
 			TableUtils.dropTable(connectionSource, Stazione.class, true);
+			TableUtils.dropTable(connectionSource, Preference.class, true);
 			// after we drop the old databases, we create the new ones
 			onCreate(db, connectionSource);
 		} catch (SQLException e) {
@@ -77,12 +95,24 @@ public class OfflineDbOpenHelper extends OrmLiteSqliteOpenHelper {
 	}
 	
 	/**
+	 * Returns the Database Access Object (DAO) for our Preferences class. It will create it or just give the cached
+	 * value.
+	 */
+	public Dao<Preference, String> getPreferencesDao() throws SQLException {
+		if (preferencesDAO == null) {
+			preferencesDAO = getDao(Preference.class);
+		}
+		return preferencesDAO;
+	}
+	
+	/**
 	 * Close the database connections and clear any cached DAOs.
 	 */
 	@Override
 	public void close() {
 		super.close();
 		stazioneDAO = null;
+		preferencesDAO = null;
 	}
 	
 	/**
@@ -91,6 +121,14 @@ public class OfflineDbOpenHelper extends OrmLiteSqliteOpenHelper {
 	 */
 	public void clearTableStazione() throws SQLException {
 		TableUtils.clearTable(getConnectionSource(), Stazione.class);
+	}
+	
+	/**
+	 * Clear preferences table
+	 * @throws SQLException 
+	 */
+	public void clearTablePreferences() throws SQLException {
+		TableUtils.clearTable(getConnectionSource(), Preference.class);
 	}
 
 }
